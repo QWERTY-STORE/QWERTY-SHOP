1a1e574e9aff4208613be7d9aa6a7c3f650817a9
VESSILL <vessill@proton.me>
2024-07-12 12:53:43 +0300

    committt

--- bot.py
+++ bot.py
@@ -5,16 +5,13 @@ bot = telebot.TeleBot('7357518923:AAGVpjjVmUBCI_-3bVdGErBqMcsU4sjJBR4')
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     try:
-        # Send a welcome message
-        bot.reply_to(message, 'Hello! Welcome to the bot.')
-
         # Create a keyboard with a button to open the Web App
         keyboard = telebot.types.InlineKeyboardMarkup()
-        url_button = telebot.types.InlineKeyboardButton(text='Open Web App', url='https://t.me/qwertyshoping_bot/SHOP')
+        url_button = telebot.types.InlineKeyboardButton(text='STORE', url='https://t.me/qwertyshoping_bot/SHOP')
         keyboard.add(url_button)
 
         # Send the keyboard to the user
-        bot.send_message(message.chat.id, 'Click the button below to open the Web App:', reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Добро пожаловать в QWERTY SHOP! Прошу Вас перейти в Web App для доступа ко всем товарам:', reply_markup=keyboard)
     except Exception as e:
         print(f'An error occurred: {e}')
 
